0820056afbb836a30ea04c39d38da7a621a2febb
unknown <yassine59958@gmail.com>
2025-08-18 12:06:06 +0200

    edit task frontend

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -5,6 +5,8 @@ function List() {
 
     const [task, setTask] = useState([]);
     const [error, setError] = useState('');
+    const [newTask, setNewTask] = useState('');
+    const [editingId, setEditingId] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:3000/todo')
@@ -26,12 +28,38 @@ function List() {
       })
     }
 
+    const starEditing = (tasks)=> {
+      setEditingId(tasks.id);
+      setNewTask(tasks.texte)
+      console.log(newTask)
+    }
+
+    const handleEdit = (id) => {
+      axios.put(`http://localhost:3000/todo/${id}`, {texte: newTask})
+      .then(() => {
+        setTask(task.map(t => t.id === id ? {...t, texte: newTask} : t))
+        setEditingId(null)
+      })
+      .catch(err => console.error(err))
+    }
+
   return (
     <>
     <h3>To do list</h3>
       <ul>
         {task.map((t) => (
-           <li key={t.id}>{t.texte} <button onClick={() => handleDelete(t.id)}>Delete</button></li>
+           <li key={t.id}>{t.texte} 
+          {editingId === t.id ? (
+            <>
+            <input value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+            <button onClick={() => handleEdit(t.id)}>Save</button>
+            <button onClick={() => setEditingId(null)}>Cancel</button>
+            </>
+
+          ) : (<><button onClick={() => starEditing(t)}>Edit</button> <button onClick={() => handleDelete(t.id)}>Delete</button></>
+
+          )}
+           </li>
         ))}
       </ul>
     </>
